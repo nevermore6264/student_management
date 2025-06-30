e8d8da7909d8e8813ad4b4c3cc7ea10d30c7496f
David <nevermore6264@example.com>
2025-06-30 18:05:32 +0700

    add

--- src/main/java/com/ute/entity/GiangVien.java
+++ src/main/java/com/ute/entity/GiangVien.java
@@ -1,13 +1,16 @@
 package com.ute.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Table(name = "giangvien")
